3e90788148220ead8a561e08cdbf84b8ab768df2
Jamie York <ziadoz@gmail.com>
2015-08-20 15:40:22 +0100

    Add basic item logistics unit tests (just exceptions, XML is included in item)

--- tests/Item/ItemLogisticsTest.php
+++ tests/Item/ItemLogisticsTest.php
@@ -6,5 +6,23 @@ use \Pangaea\Item\ItemLogistics;
 
 class ItemLogisticsTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @expectedException        \Pangaea\PangaeaException
+     * @expectedExceptionMessage Unit cost currency cannot be blank
+     */
+    public function testBlankItemLogisticsCurrencyException()
+    {
+        $itemLogistics = new ItemLogistics;
+        $itemLogistics->setUnitCost(123.99, '');
+    }
 
+    /**
+     * @expectedException              \Pangaea\PangaeaException
+     * @expectedExceptionMessageRegExp /Invalid unit cost currency ".*"/
+     */
+    public function testInvalidItemLogisticsCurrencyException()
+    {
+        $itemLogistics = new ItemLogistics;
+        $itemLogistics->setUnitCost(123.99, 'FOOBAR');
+    }
 }
