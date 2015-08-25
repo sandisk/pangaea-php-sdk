407162ff81d08cd2c72fabe340415c285195dbcc
Jamie York <ziadoz@gmail.com>
2015-08-25 11:50:29 +0100

    Added test for partial product attributes plus count checks

--- tests/Item/ItemLogisticsTest.php
+++ tests/Item/ItemLogisticsTest.php
@@ -14,9 +14,21 @@ class ItemLogisticsTest extends \PHPUnit_Framework_TestCase
 
         $attributes = $itemLogistics->getProductAttributes();
 
-        $this->assertEquals(12345,    $attributes[0]->getValue()[0]);
+        $this->assertCount(3, $attributes);
+        $this->assertEquals(12345, $attributes[0]->getValue()[0]);
         $this->assertEquals(12345678, $attributes[1]->getValue()[0]);
-        $this->assertEquals(123456,   $attributes[2]->getValue()[0]);
+        $this->assertEquals(123456, $attributes[2]->getValue()[0]);
+    }
+
+    public function testItemLogisticsPartialProductAttributes()
+    {
+        $itemLogistics = new ItemLogistics;
+        $itemLogistics->setLegacyDistributorId(12345);
+
+        $attributes = $itemLogistics->getProductAttributes();
+
+        $this->assertCount(1, $attributes);
+        $this->assertEquals(12345, $attributes[0]->getValue()[0]);
     }
 
     public function testItemLogisticsBlankProductAttributes()
